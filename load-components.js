1a20ad59ca5fac692c6f036d8bc2a9b04ab72bd0
hayleemelia <h.ayers@acg.edu>
2026-03-03 04:08:04 +0200

    Making hamburger menu on navbar for mobile

--- load-components.js
+++ load-components.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
-    $("#nav-placeholder").load("navbar.html");
+
+    $("#nav-placeholder").load("navbar.html", function() {
+        const $logo = $("#nav-logo");
+        const $navLinks = $(".nav-links");
+
+        $logo.on("click", function() {
+            $navLinks.toggleClass("active"); // toggle the menu
+        });
+    });
+
     $("#footer-placeholder").load("footer.html");
 });
